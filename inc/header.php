67dfb540f40da8e7fcf706d3a52f16c8ad9dd207
Čedomir Ivanović <chedaivanovic@gmail.com>
2022-04-10 14:46:32 +0200

    Google Analytics

--- inc/header.php
+++ inc/header.php
@@ -60,6 +60,18 @@
             "sameAs": []
         }
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WBN7087GJC"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-WBN7087GJC');
+    </script>
     <?php
     require('functions.php');
     ?>
